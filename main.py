139767488970193de0dd7fe7c5a823c48102cb1a
dbuawork <dbuawork@gmail.com>
2023-12-29 19:25:01 +0200

    firts-tasks--negative_added

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ random_numbers = [random.randint(-100, 100) for _ in range(10)]
 # Виведення списку
 print("Список випадкових чисел:", random_numbers)
 
+# Cумма негативних чисел
+negative = sum(x for x in random_numbers if x < 0)
+print("Сума негативних чисел:", negative)
+
